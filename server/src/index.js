19017e23efee74adfd309a39e3ec2622517838d1
43MorenoLuis <lmoreno9943@gmail.com>
2021-10-27 20:12:55 -0500

    Conectar Node a MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { dbConnection } = require('./database/config');
 require ('dotenv').config();
 // Crear el servidor de express
 const app = express();
+
+// Base de datos
+dbConnection();
 
 // Directorio Publico
 app.use( express.static('public'));
@@ -14,5 +18,5 @@ app.use('/api/auth', require('./routes/auth') );
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () =>{
-    console.log(`Servidor on port at ${process.env.PORT}`);
-})
+    console.log(`Servidor on port at ${process.env.PORT} 💻`);
+});
