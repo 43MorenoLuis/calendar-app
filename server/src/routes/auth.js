77a79959739754c331adc2e9fa3aa74b62cebd15
43MorenoLuis <lmoreno9943@gmail.com>
2021-10-27 19:03:42 -0500

    Custom Middleware

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -6,20 +6,22 @@
 const { Router } = require('express');
 const router = Router();
 const { check } = require('express-validator');
-
+const { validateFields } = require('../middlewares/validate-fields');
 const { createUser, loginUser, revalidateToken } = require ('../controllers/auth');
 
 router.post('/new', [
     // middlewares
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 })
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 }),
+    validateFields
 ], createUser);
 
 router.post('/', [
     // middlewares
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 })
+    check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 }),
+    validateFields
 ], loginUser);
 
 router.get('/new', revalidateToken);
