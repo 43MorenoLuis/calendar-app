9e3feabc223e4abe546bf6accf45fd5817fc3fd3
43MorenoLuis <lmoreno9943@gmail.com>
2021-11-01 14:52:58 -0500

    Peticion HTTP para autenticarnos

--- client/src/components/auth/LoginScreen.js
+++ client/src/components/auth/LoginScreen.js
@@ -1,75 +1,105 @@
 import React from 'react';
 import './login.css';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch } from 'react-redux';
+import { startLogin } from '../../actions/auth';
 
 export const LoginScreen = () => {
+
+    // const initialForm = {
+    //     name: '',
+    //     age: 0,
+    //     email: ''
+    // };
+    const dispatch = useDispatch();
+
+    const [ formLoginValues, handleLoginInputChange ] = useForm({
+        lEmail: 'lmoreno9943@gmail.com',
+        lPassword: 'Luis123456'
+    });
+
+    const { lEmail, lPassword } = formLoginValues;
+
+    const handleLogin = ( e ) => {
+        e.preventDefault();
+        dispatch( startLogin( lEmail, lPassword ) );
+    }
+
+
     return (
-        <div className="container login-container">
-            <div className="row">
-                <div className="col-md-6 login-form-1">
+        <div className='container login-container'>
+            <div className='row'>
+                <div className='col-md-6 login-form-1'>
                     <h3>Ingreso</h3>
-                    <form>
-                        <div className="form-group">
+                    <form onSubmit={ handleLogin }>
+                        <div className='form-group'>
                             <input 
-                                type="text"
-                                className="form-control"
-                                placeholder="Correo"
+                                type='text'
+                                className='form-control'
+                                placeholder='Correo'
+                                name='lEmail'
+                                value={ lEmail }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
-                        <div className="form-group">
+                        <div className='form-group'>
                             <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña"
+                                type='password'
+                                className='form-control'
+                                placeholder='Contraseña'
+                                name='lPassword'
+                                value={ lPassword }
+                                onChange={ handleLoginInputChange }
                             />
                         </div>
-                        <div className="form-group">
+                        <div className='form-group'>
                             <input 
-                                type="submit"
-                                className="btnSubmit"
-                                value="Login" 
+                                type='submit'
+                                className='btnSubmit'
+                                value='Login' 
                             />
                         </div>
                     </form>
                 </div>
 
-                <div className="col-md-6 login-form-2">
+                <div className='col-md-6 login-form-2'>
                     <h3>Registro</h3>
                     <form>
-                        <div className="form-group">
+                        <div className='form-group'>
                             <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Nombre"
+                                type='text'
+                                className='form-control'
+                                placeholder='Nombre'
                             />
                         </div>
-                        <div className="form-group">
+                        <div className='form-group'>
                             <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Correo"
+                                type='email'
+                                className='form-control'
+                                placeholder='Correo'
                             />
                         </div>
-                        <div className="form-group">
+                        <div className='form-group'>
                             <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Contraseña" 
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Repita la contraseña" 
+                                type='password'
+                                className='form-control'
+                                placeholder='Contraseña' 
                             />
                         </div>
 
-                        <div className="form-group">
+                        <div className='form-group'>
+                            <input
+                                type='password'
+                                className='form-control'
+                                placeholder='Repita la contraseña' 
+                            />
+                        </div>
+
+                        <div className='form-group'>
                             <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
+                                type='submit' 
+                                className='btnSubmit' 
+                                value='Crear cuenta' />
                         </div>
                     </form>
                 </div>
