0574575c6a1d54a9a85e83c001fb854dad8a2bb8
43MorenoLuis <lmoreno9943@gmail.com>
2021-11-01 16:24:12 -0500

    Mantener el estado de la autenticazion

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import Swal from "sweetalert2";
-import { fetchSinToken } from "../helpers/fetch"
+import { fetchConToken, fetchSinToken } from "../helpers/fetch"
 import { types } from "../types/types";
 
 export const startLogin = ( email, password ) => {
@@ -41,6 +41,31 @@ export const startRegister = ( name, email, password ) => {
         }
     }
 }
+
+export const startChecking = () => {
+    return async( dispatch ) => {
+
+        const resp = await fetchConToken( 'auth/renew' );
+        const body = await resp.json();
+
+        if( body.ok ){
+            localStorage.setItem('token', body.token );
+            localStorage.setItem( 'token-init-date', new Date().getTime() );
+        
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+            }) );
+        }else{
+            Swal.fire('Error', body.msg, 'error');
+            dispatch( checkingFinish() );
+        }
+    }
+}
+
+const checkingFinish = () => ({ 
+    type: types.authCheckingFinish 
+});
 
 const login = ( user ) => ({
     type: types.authLogin,
